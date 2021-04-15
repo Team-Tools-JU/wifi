f05217286d926041c41e8262dd8141dbc8689f83
Oskar <oskar11.persson@gmail.com>
2021-04-15 15:00:39 +0200

    test program for uart communcation working

--- uart_test.py
+++ uart_test.py
@@ -3,11 +3,12 @@ from time import sleep
 
 ser = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
 
-ser.write("test")
+bs = bytearray("testcode")
+ser.write(bs)
 while True:
     received_data = ser.read()              #read serial port
     sleep(0.03)
     data_left = ser.inWaiting()             #check for remaining byte
     received_data += ser.read(data_left)
     print (received_data)                   #print received data
-    ser.write(received_data)                #transmit data serially 
+    #ser.write(received_data)               #transmit data serially 
