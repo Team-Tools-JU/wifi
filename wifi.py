2d1816e3825bf7be9da02ff2f195e2cb0217a28d
Lepuff <oskar11.persson@gmail.com>
2021-05-06 15:56:29 +0200

    added threading

--- wifi.py
+++ wifi.py
@@ -1,12 +1,11 @@
 import serial
 from time import sleep
-
+import threading
 ser = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
 bufferReady = True
-
 buffer = []
 
-def runServer():
+def sendLoop():
     global buffer
     while True:
         if buffer.count > 0 and bufferReady:
@@ -16,19 +15,22 @@ def runServer():
             bufferReady = True
 
 
-while True:
-    if bufferReady:
-        bufferReady = False
-        received_data = ser.read()              #read serial port
-        sleep(0.03)
-        data_left = ser.inWaiting()             #check for remaining byte
-        received_data += ser.read(data_left)
-        decoded_data = received_data.decode("utf-8")
-        buffer.append(decoded_data)
-        bufferReady = True
+def readLoop():
+    while True:
+        if bufferReady:
+            bufferReady = False
+            received_data = ser.read()              #read serial port
+            sleep(0.03)
+            data_left = ser.inWaiting()             #check for remaining byte
+            received_data += ser.read(data_left)
+            decoded_data = received_data.decode("utf-8")
+            buffer.append(decoded_data)
+            bufferReady = True
 
 
+def setup():
+    threading.Thread(target=readLoop,args=())
+    threading.Thread(target=sendLoop,args=())
 
 
-
-
+setup()
